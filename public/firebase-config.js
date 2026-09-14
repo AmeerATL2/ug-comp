04bf4ed072f42ae8208649dc7099cf4f71cf058d
AmeerATL2 <ameer.bell@hgs.hiddengeniusproject.org>
2026-09-14 18:48:04 -0400

    Add files via upload

---
+++ public/firebase-config.js
@@ -0,0 +1,8 @@
+window.UG_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAhz5CMsXkedLicBmJiIUzkaySTkWuHE14",
+  authDomain: "ug-comp.firebaseapp.com",
+  projectId: "ug-comp",
+  storageBucket: "ug-comp.firebasestorage.app",
+  messagingSenderId: "985313520722",
+  appId: "1:985313520722:web:68e2c9e54ff06bc91a838b"
+};
